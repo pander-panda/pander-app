1f7b7c0d0b379c17c1202a8b4ed8bf9dbe2c402a
obi <sam.nnodim@gmail.com>
2018-05-09 00:15:22 -0700

    basic outline in place

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  Image,
   StyleSheet,
-  ImageBackground
+  ImageBackground,
+  TouchableOpacity,
+  TextInput,
 } from 'react-native';
 
 class LoginScreen extends Component{
@@ -13,16 +16,78 @@ class LoginScreen extends Component{
   }
 
   render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <ImageBackground
-          source={require('../assets/login_bg.jpg')}
-          style={{ flex: 1 }}
-        >
+    return <View style={{ flex: 1 }}>
+        <ImageBackground source={require("../assets/login_bg.jpg")} style={{ flex: 1 }}>
+          {/* Pander Logo Box */}
+          <View style={{ flex: 1, justifyContent:'center', alignItems:'center' }} >
+            <View style={{ backgroundColor: 'white', 
+            height: 125, width: 125, alignItems:'center', justifyContent:'center' }} >
+              <Text style={{ fontWeight: 'bold', fontSize: 24 }}>PANDER</Text>
+            </View>
+          </View>
+          
+          {/* Bottom Half */}
+          <View>
 
+            {/* 'Get stuff done' section */}
+            <View style={{ height: 150, backgroundColor: 'white' }}>
+              <View style={{
+                opacity:1, //animated
+                alignItems:'flex-start',
+                paddingHorizontal: 25,
+                marginTop: 25, //animated
+              }}>
+                <Text style={{ fontSize: 22 }}>Get stuff done, with Pander</Text>
+              </View>
+
+              <TouchableOpacity>
+                <View style={{
+                  marginTop:25,
+                  paddingHorizontal:25,
+                  flexDirection:'row'
+                }}>
+                  <Image
+                    source={require('../assets/nigeria.jpg')}
+                    style={{height:24,width:24,resizeMode:'contain'}} 
+                  />
+                  <View
+                    style={{flexDirection:'row', flex:1, }} >
+                    {/* Country Code */}
+                    <Text style={{ fontSize:16, paddingHorizontal: 10}}>+234</Text>
+                    {/* Phone Number Input */}
+                    <TextInput 
+                      style={{flex:1, fontSize:16}}
+                      placeholder='Enter your mobile number'
+                      underlineColorAndroid='transparent'
+                    />
+                  </View>
+                  
+                </View> 
+            </TouchableOpacity>
+            </View>
+
+            {/* 'Connect with social' section */}
+            <View style={{
+              height: 50,
+              backgroundColor:'white',
+              alignItems:'center',
+              justifyContent:'center',
+              borderTopColor: '#e8e8ec',
+              borderTopWidth:1,
+              paddingHorizontal: 25
+            }}>
+              <Text
+                style={{
+                  color:'#000',
+                  fontWeight:'bold'
+                }}
+              >
+                Or connect using a social account
+              </Text>
+            </View>
+          </View>
         </ImageBackground>
-      </View>
-    )
+      </View>;
   }
 }
 
